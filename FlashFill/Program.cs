5938ad73bf74a47328c0d44eb06de05881013696
David Khachaturov <d77777k@gmail.com>
2020-08-02 08:02:39 +0100

    Initial Commit (own regex)

---
+++ FlashFill/Program.cs
@@ -0,0 +1,312 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// https://www.microsoft.com/en-us/research/wp-content/uploads/2016/12/popl11-synthesis.pdf
+namespace FlashFill {
+	public static class Tokens {
+		// https://github.com/MikaelMayer/StringSolver/blob/2d3e4476c5fe71a4b17387dd68f8c772c52dcdc2/src/main/scala/ch/epfl/lara/synthesis/stringsolver/ScalaRegExp.scala#L84
+		// Canonical to paper
+		public static Regex NumTok = new Regex(@"\d+");
+		public static Regex AlphTok = new Regex(@"[a-zA-Z]+");
+		public static Regex LowerTok = new Regex(@"[a-z]+");
+		public static Regex UpperTok = new Regex(@"[A-Z]+");
+		public static Regex AccentTok = new Regex(@"[\u00C0-\u00FF]+");
+		public static Regex AlphNumTok = new Regex(@"[a-zA-Z0-9]+");
+		public static Regex WhiteSpaceTok = new Regex(@"\s+");
+		public static Regex CommaTok = new Regex(@",+");
+		public static Regex SlashTok = new Regex(@"\\+");
+		public static Regex AnyTok = new Regex(@".+");
+		public static Regex Epsilon = new Regex(@"");
+		public static Regex NonWhiteSpaceTok = new Regex(@"\S+");
+		// "[^\.]+"
+
+		// Extra
+		public static Regex DotTok = new Regex(@"\.+");
+
+		public static Regex TokenSeq(Regex r1, Regex r2) {
+			return new Regex(r1.ToString() + r2.ToString());
+		}
+
+		public static Regex NonTokenSeq(Regex r1, Regex r2) {
+			return new Regex(@"\b" + r1.ToString() + r2.ToString() + @"\b");
+		}
+
+		public static Regex Non(Regex r) {
+			return new Regex(@"(?!" + r.ToString() + @")");
+		}
+	}
+
+	public abstract class Position {
+		public abstract int? Eval(string s);
+	}
+
+	public abstract class Expression {
+		public abstract string Eval(string s);
+
+		public abstract string Eval();
+	}
+
+	public abstract class TraceExpression {
+		public abstract string Eval(string s);
+
+		public abstract string Eval();
+	}
+
+	public abstract class AtomicExpression {
+		public abstract string Eval(string s);
+
+		public abstract string Eval();
+	}
+
+	public abstract class Conditional {
+		public abstract bool Eval(string s);
+
+		public abstract bool Eval();
+	}
+
+	public class CPos : Position {
+		int k;
+		public CPos(int k) {
+			this.k = k;
+		}
+
+		public override int? Eval(string s) {
+			if (k < 0)
+				return s.Length + k + 1;
+			return k;
+		}
+	}
+
+	public class Pos : Position {
+		Regex r_1, r_1s, r_2, r_2s;
+		int c;
+		private bool IsEps(Regex r) {
+			return r.ToString() == Tokens.Epsilon.ToString();
+		}
+
+		public Pos(Regex r_1, Regex r_2, int cp) {
+			if (cp > 0) {
+				this.r_1 = new Regex(r_1.ToString());
+				this.r_2 = r_2;
+				this.r_2s = new Regex("^" + r_2.ToString());
+				this.c = cp;
+			} else {
+				this.r_1 = new Regex(r_1.ToString() + "$");
+				this.r_2 = new Regex(r_2.ToString(), RegexOptions.RightToLeft);
+				this.c = cp;
+			}
+		}
+
+		public override int? Eval(string s) {
+			int t;
+			int c = this.c;
+			if (c > 0) {
+				var exclusions = r_2.Matches(s).Select(x => Tuple.Create(x.Index, x.Index + x.Length));
+				foreach (System.Text.RegularExpressions.Match match in r_1.Matches(s)) {
+					t = match.Index + match.Length;
+
+					bool skip = false;
+					if (IsEps(r_1)) {
+						foreach (var ex in exclusions) {
+							if (t > ex.Item1 && t <= ex.Item2) {
+								skip = true; break;
+							}
+						}
+					}
+
+					if (!skip && r_2s.Match(s.Substring(t)).Success) {
+						c--;
+						if (c > 0)
+							continue;
+						return t;
+					}
+				}
+			} else if (c < 0) {
+				c = -c;
+				foreach (System.Text.RegularExpressions.Match match in r_2.Matches(s)) {
+					t = match.Index;
+					if (r_1.Match(s.Substring(0, t)).Success) {
+						c--;
+						if (c > 0)
+							continue;
+						return t;
+					}
+				}
+			}
+			return null;
+		}
+	}
+
+	public class SubStr : AtomicExpression {
+		string v_i;
+		Position p_1, p_2;
+		public SubStr(string v_i, Position p_1, Position p_2) {
+			this.v_i = v_i; this.p_1 = p_1; this.p_2 = p_2;
+		}
+
+		public override string Eval() {
+			int i1 = p_1.Eval(v_i) ?? -1;
+			int i2 = p_2.Eval(v_i) ?? -1;
+			if (i1 >= v_i.Length || i1 < 0 || i2 > v_i.Length || i2 < 0)
+				return null;
+
+			return v_i[i1..i2];
+		}
+
+		public override string Eval(string v) {
+			int i1 = p_1.Eval(v) ?? -1;
+			int i2 = p_2.Eval(v) ?? -1;
+			if (i1 >= v.Length || i1 < 0 || i2 > v.Length || i2 < 0)
+				return null;
+
+			return v[i1..i2];
+		}
+	}
+
+	public class SubStr2 : AtomicExpression {
+		string v_i;
+		Regex r;
+		int c;
+		public SubStr2(string v_i, Regex r, int c) {
+			this.v_i = v_i; this.r = r; this.c = c;
+		}
+
+		public override string Eval() {
+			return new SubStr(v_i, new Pos(Tokens.Epsilon, r, c), new Pos(r, Tokens.Epsilon, c)).Eval();
+		}
+
+		public override string Eval(string v) {
+			return new SubStr(v, new Pos(Tokens.Epsilon, r, c), new Pos(r, Tokens.Epsilon, c)).Eval();
+		}
+	}
+
+	public class ConstStr : AtomicExpression {
+		string s;
+		public ConstStr(string s) {
+			this.s = s;
+		}
+
+		public override string Eval(string _) {
+			return s;
+		}
+
+		public override string Eval() {
+			return s;
+		}
+	}
+
+	public class Loop : AtomicExpression {
+		Func<int, TraceExpression> e;
+
+		public Loop(Func<int, TraceExpression> e) {
+			this.e = e;
+		}
+
+		public override string Eval(string s) {
+			return new LoopR(e, 1, s).Eval();
+		}
+
+		public override string Eval() {
+			throw new Exception("Loop needs string to be evaluatued");
+		}
+	}
+
+	public class LoopR {
+		Func<int, TraceExpression> e;
+		int k;
+		string s;
+
+		public LoopR(Func<int, TraceExpression> e, int k, string s) {
+			this.e = e; this.k = k; this.s = s;
+		}
+
+		public string Eval() {
+			string t = e(k).Eval(s);
+			if (t == null)
+				return null;
+			return t + (new LoopR(e, k + 1, s).Eval());
+		}
+	}
+
+	public class Match : Conditional {
+		string v_i;
+		Regex r;
+		int k;
+
+		public Match(string v_i, Regex r, int k) {
+			this.v_i = v_i; this.r = r; this.k = k;
+		}
+
+		public override bool Eval() {
+			return r.Matches(v_i).Count >= k;
+		}
+
+		public override bool Eval(string v) {
+			return r.Matches(v).Count >= k;
+		}
+	}
+
+	public class Switch : Expression {
+		List<Tuple<Conditional, TraceExpression>> tuples;
+
+		public Switch(List<Tuple<Conditional, TraceExpression>> tuples) {
+			this.tuples = tuples;
+		}
+
+		public override string Eval() {
+			foreach (Tuple<Conditional, TraceExpression> t in tuples) {
+				if (t.Item1.Eval())
+					return t.Item2.Eval();
+			}
+			return null;
+		}
+
+		public override string Eval(string s) {
+			foreach (Tuple<Conditional, TraceExpression> t in tuples) {
+				if (t.Item1.Eval(s))
+					return t.Item2.Eval(s);
+			}
+			return null;
+		}
+	}
+
+	public class Concatenate : TraceExpression {
+		List<AtomicExpression> fs;
+
+		public Concatenate(List<AtomicExpression> fs) {
+			this.fs = fs;
+		}
+
+		public override string Eval() {
+			StringBuilder stringBuilder = new StringBuilder();
+			foreach (AtomicExpression f in fs) {
+				string t = f.Eval();
+				if (t == null)
+					return null;
+				stringBuilder.Append(t);
+			}
+			return stringBuilder.ToString();
+		}
+
+		public override string Eval(string s) {
+			StringBuilder stringBuilder = new StringBuilder();
+			foreach (AtomicExpression f in fs) {
+				string t = f.Eval(s);
+				if (t == null)
+					return null;
+				stringBuilder.Append(t);
+			}
+			return stringBuilder.ToString();
+		}
+	}
+
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ FlashFillTests/BaseTests.cs
@@ -0,0 +1,137 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlashFill;
+using System.Text.RegularExpressions;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Net;
+using System;
+using System.Numerics;
+using System.Text;
+
+namespace FlashFillTests {
+	[TestClass]
+	public class BaseTests {
+		// https://github.com/MikaelMayer/StringSolver/blob/master/src/test/scala/ch/epfl/lara/synthesis/stringsolver/BaseTests.scala#L63
+		[TestMethod]
+		public void SubStrTest() {
+			string v = "abcdefg";
+			SubStr ss = new SubStr(v, new CPos(0), new CPos(-1));
+			Assert.AreEqual(v, ss.Eval());
+
+			v = "qsdf1234amlkj12345mlkj432  fkj ";
+			ss = new SubStr(v, new Pos(Tokens.NumTok, Tokens.AlphTok, 2), new CPos(-1));
+			Assert.AreEqual("mlkj432  fkj ", ss.Eval());
+
+			v = "CAMP DRY DBL NDL 3.6 OZ";
+			ss = new SubStr(v, new Pos(Tokens.Epsilon, Tokens.NumTok, 1), new CPos(-1));
+			Assert.AreEqual("3.6 OZ", ss.Eval());
+
+			v = "My Taylor is Rich";
+			SubStr2 ss2 = new SubStr2(v, Tokens.UpperTok, 2);
+			Assert.AreEqual("T", ss2.Eval());
+			ss2 = new SubStr2(v, Tokens.UpperTok, 3);
+			Assert.AreEqual("R", ss2.Eval());
+
+			v = "Dr. Eran Yahav";
+			SubStr firstname = new SubStr(v, new Pos(Tokens.Epsilon, Tokens.NonTokenSeq(Tokens.AlphTok, Tokens.Non(Tokens.DotTok)), 1),
+				new Pos(Tokens.Epsilon, Tokens.TokenSeq(Tokens.LowerTok, Tokens.Non(Tokens.DotTok)), 1));
+			Assert.AreEqual("E", firstname.Eval());
+
+			ss2 = new SubStr2(v, Tokens.AlphTok, -1);
+			Assert.AreEqual("Yahav", ss2.Eval());
+
+			v = "Prof. Kathleen S. Fisher";
+			firstname = new SubStr(v, new Pos(Tokens.Epsilon, Tokens.NonTokenSeq(Tokens.AlphTok, Tokens.Non(Tokens.DotTok)), 1),
+				new Pos(Tokens.Epsilon, Tokens.NonTokenSeq(Tokens.LowerTok, Tokens.Non(Tokens.DotTok)), 1));
+			Assert.AreEqual("K", firstname.Eval());
+		}
+
+		[TestMethod]
+		public void LoopTest() {
+			string v = "qsdf1234amlkj12345mlkj432  fkj ";
+			var ss = new Concatenate(new List<AtomicExpression> { new ConstStr(" + "),
+				new SubStr(v, new Pos(Tokens.NumTok, Tokens.AlphTok, 2), new CPos(-1)) });
+			Assert.AreEqual(" + mlkj432  fkj ", ss.Eval());
+
+			v = "My Taylor is Rich";
+			Loop l = new Loop((int w) => {
+				return new Concatenate(new List<AtomicExpression> { new SubStr2(v, Tokens.UpperTok, w) });
+			});
+			Assert.AreEqual("MTR", l.Eval(v));
+		}
+
+		[TestMethod]
+		public void Example2() {
+			string v = "";
+			SubStr e2 = new SubStr(v, new Pos(Tokens.Epsilon, Tokens.NumTok, 1), new CPos(-1));
+
+			Assert.AreEqual("15Z", e2.Eval("BTR KRNL WK CORN 15Z"));
+			Assert.AreEqual("3.6 OZ", e2.Eval("CAMP DRY DBL NDL 3.6 OZ"));
+			Assert.AreEqual("1 PK", e2.Eval("CHORE BOY HD SC SPNG 1 PK"));
+			Assert.AreEqual("5 Z", e2.Eval("FRENCH WORCESTERSHIRE 5 Z"));
+			Assert.AreEqual("6 OZ", e2.Eval("O F TOMATO PASTE 6 OZ"));
+		}
+
+		[TestMethod]
+		public void Example3() {
+			string v = "";
+			SubStr e3 = new SubStr(v, new CPos(0), new Pos(Tokens.SlashTok, Tokens.Epsilon, -1));
+
+			Assert.AreEqual("Company\\Code\\", e3.Eval("Company\\Code\\Index.html"));
+			Assert.AreEqual("Company\\Docs\\Spec\\", e3.Eval("Company\\Docs\\Spec\\specs.doc"));
+		}
+
+		[TestMethod]
+		public void Example6() {
+			string v = "        Oege    de    Moor";
+			var ss = new Concatenate(new List<AtomicExpression> {
+				new Loop((int w) => {
+					return new Concatenate(new List<AtomicExpression> { new SubStr(v,
+						new Pos(Tokens.Epsilon, Tokens.NonWhiteSpaceTok, w),
+						new Pos(Tokens.NonWhiteSpaceTok, Tokens.TokenSeq(Tokens.WhiteSpaceTok, Tokens.NonWhiteSpaceTok), w)),
+						new ConstStr(" ")
+					});
+				}),
+				new SubStr2(v, Tokens.NonWhiteSpaceTok, -1)
+			});
+			Assert.AreEqual("Oege de Moor", ss.Eval(v));
+		}
+
+		[TestMethod]
+		public void Example9() {
+			string v = "Prof. Kathleen S. Fisher";
+			SubStr firstname = new SubStr(v, new Pos(Tokens.Epsilon, Tokens.NonTokenSeq(Tokens.AlphTok, Tokens.Non(Tokens.DotTok)), 1),
+				new Pos(Tokens.Epsilon, Tokens.NonTokenSeq(Tokens.LowerTok, Tokens.Non(Tokens.DotTok)), 1));
+			Concatenate e1 = new Concatenate(new List<AtomicExpression> {
+				new SubStr(v, new Pos(Tokens.Epsilon, Tokens.TokenSeq(Tokens.AlphTok, Tokens.CommaTok), 1),
+				new Pos(Tokens.AlphTok, Tokens.CommaTok, 1)),
+				new ConstStr(", "), firstname, new ConstStr(".")
+			});
+			Concatenate e2 = new Concatenate(new List<AtomicExpression> {
+				new SubStr2(v, Tokens.AlphTok, -1), new ConstStr(", "), firstname, new ConstStr(".")
+			});
+			Switch swi = new Switch(new List<Tuple<Conditional, TraceExpression>> {
+				Tuple.Create<Conditional, TraceExpression>(new FlashFill.Match(v, Tokens.CommaTok, 1), e1),
+				Tuple.Create<Conditional, TraceExpression>(new FlashFill.Match(v, Tokens.Non(Tokens.CommaTok), 1), e2)
+			});
+
+			Assert.AreEqual("Yahav, E.", swi.Eval("Dr. Eran Yahav"));
+			Assert.AreEqual("Fisher, K.", swi.Eval("Prof. Kathleen S. Fisher"));
+			Assert.AreEqual("Gates, B.", swi.Eval("Bill Gates, Sr."));
+			Assert.AreEqual("Necula, G.", swi.Eval("George Ciprian Necula"));
+			Assert.AreEqual("McMillan, K.", swi.Eval("Ken McMillan, II"));
+		}
+
+		[TestMethod]
+		public void Example12() {
+			Func<string, string, string> e12 = (string v1, string v2) => {
+				return new Concatenate(new List<AtomicExpression> {
+					new ConstStr("case "), new ConstStr(v2), new ConstStr(": return “"), new ConstStr(v1), new ConstStr("”;")
+				}).Eval();
+			};
+
+			Assert.AreEqual("case 355: return “Albania”;", e12("Albania", "355"));
+			Assert.AreEqual("case 213: return “Algeria”;", e12("Algeria", "213"));
+		}
+	}
+}
